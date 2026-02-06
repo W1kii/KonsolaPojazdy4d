63e11ef4074ced6e9856870d3f5c4d6247e8ebff
Student <Student@TE-KAL-D-S14-03>
2026-02-06 13:00:11 +0100

    Dodanie klasy pojazd i samochód.

---
+++ KonsolaPojazdy4d/Pojazd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolaPojazdy4d
+{
+    internal abstract class Pojazd
+    {
+
+        protected string marka;
+        protected ushort rokProdukcji;
+
+        public Pojazd(string marka, ushort rokProdukcji)
+        { 
+            this.marka = marka;
+            this.rokProdukcji = rokProdukcji;
+        }
+
+        public abstract double ObliczKosztWynajmu(int dni);
+
+        public virtual string Opis() 
+        {
+            return $"Marka: {this.marka}, Rok produkcji: {this.rokProdukcji}";
+        }
+    }
+}
---
+++ KonsolaPojazdy4d/Samochod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolaPojazdy4d
+{
+    internal class Samochod : Pojazd
+    {
+
+        private int liczbaDrzwi;
+
+        public Samochod(string marka, ushort rokProdukcji, int liczbaDrzwi) : base ( marka, rokProdukcji) 
+        {
+            this.liczbaDrzwi = liczbaDrzwi;
+        }
+
+        public override double ObliczKosztWynajmu(int dni)
+        {
+            return 100.0 * dni;
+        }
+        public override string Opis()
+        {
+            return base.Opis() + $" liczba drzwi: {this.liczbaDrzwi}";
+        }
+    }
+}
